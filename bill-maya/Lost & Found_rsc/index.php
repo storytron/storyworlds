2c03f2227674ff7e9c04993674a065b570549f31
Bill Maya <bill.maya@gmail.com>
2018-11-22 21:06:15 -0500

    Initial commit.

---
+++ bill-maya/Lost & Found_rsc/index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<link rel="shortcut icon" href="favicon.ico" />
+<title>Board Message</title>
+ 
+<style type="text/css" media="all">
+
+@import url(http://www.storytron.com/ipb/style_images/css_2.css);
+
+</style> 
+ 
+</head> 
+<body>
+<div id="ipbwrapper">
+<!--ipb.javascript.start-->
+<script type="text/javascript">
+ //<![CDATA[
+ var ipb_var_st            = "0";
+ var ipb_lang_tpl_q1       = "Please enter a page number to jump to between 1 and";
+ var ipb_var_s             = "fc90e00f9e7cf08ea219ffc0872ba5d6";
+ var ipb_var_phpext        = "php";
+ var ipb_var_base_url      = "http://www.storytron.com/ipb/index.php?s=&";
+ var ipb_var_image_url     = "style_images/1";
+ var ipb_input_f           = "0";
+ var ipb_input_t           = "0";
+ var ipb_input_p           = "0";
+ var ipb_var_cookieid      = "";
+ var ipb_var_cookie_domain = "";
+ var ipb_var_cookie_path   = "";
+ var ipb_md5_check         = "478e4bcaa0e99aa3375a3c7baa577cc8";
+ var ipb_new_msgs          = 0;
+ var use_enhanced_js       = 1;
+ var use_charset           = "iso-8859-1";
+ var ipb_myass_chars_lang  = "Not enough characters";
+ var ajax_load_msg		   = "Loading Content...";
+ //]]>
+</script>
+<script type="text/javascript" src='jscripts/ips_ipsclass.js'></script>
+<script type="text/javascript" src='jscripts/ipb_global.js'></script>
+<script type="text/javascript" src='jscripts/ips_menu.js'></script>
+<script type="text/javascript" src='style_images/1/folder_js_skin/ips_menu_html.js'></script>
+<script type="text/javascript" src='cache/lang_cache/en/lang_javascript.js'></script>
+<script type="text/javascript">
+//<![CDATA[
+var ipsclass = new ipsclass();
+ipsclass.init();
+ipsclass.settings['do_linked_resize'] = parseInt( "1" );
+ipsclass.settings['resize_percent']   = parseInt( "50" );
+//]]>
+</script>
+<!--ipb.javascript.end-->
+<div class="borderwrap">
+	<div id="logostrip"><a href='http://www.storytron.com/ipb/index.php?'><!--ipb.logo.start--><img src='style_images/2_logo-storyboard.jpg' alt='IPB' style='vertical-align:top' border='0' /><!--ipb.logo.end--></a></div>
+	<div id="submenu">
+		<!--ipb.leftlinks.start-->
+		
+			<div class='ipb-top-left-link'><a href="http://www.storytron.com/">------------------------------  Visit Storytron's home page</a></div>
+		
+				
+		<!--IBF.RULES-->
+		<!--ipb.leftlinks.end-->
+		<!--ipb.rightlinks.start-->
+		<div class='ipb-top-right-link'><a href="http://www.storytron.com/ipb/index.php?act=Help">Help</a></div>
+		<div class='ipb-top-right-link' id="ipb-tl-search"><a href="http://www.storytron.com/ipb/index.php?act=Search&amp;f=0">Search</a></div>
+		<div class='ipb-top-right-link'><a href="http://www.storytron.com/ipb/index.php?act=Members">Members</a></div>
+		<div class='ipb-top-right-link'><a href="http://www.storytron.com/ipb/index.php?act=calendar">Calendar</a></div>
+		
+		<div class='popupmenu-new' id='ipb-tl-search_menu' style='display:none;width:210px'>
+			<form action="http://www.storytron.com/ipb/index.php?act=Search&amp;CODE=01" method="post">
+				<input type='hidden' name='forums' id='gbl-search-forums' value='all' /> 
+				<input type="text" size="20" name="keywords" id='ipb-tl-search-box' />
+				<input class="button" type="image" style='border:0px' src="style_images/1/login-button.gif" />
+				
+			</form>
+			<div style='padding:4px'>
+				<a href='http://www.storytron.com/ipb/index.php?act=Search'>More Search Options</a>
+			</div>
+		</div>
+		<script type="text/javascript">
+			ipsmenu.register( "ipb-tl-search", 'document.getElementById("ipb-tl-search-box").focus();' );
+			gbl_check_search_box();
+		</script>
+		<!--ipb.rightlinks.end-->
+	</div>
+</div>
+
+<script type="text/javascript" src='jscripts/ips_xmlhttprequest.js'></script>
+<script type="text/javascript" src='jscripts/ipb_global_xmlenhanced.js'></script>
+<script type="text/javascript" src='jscripts/dom-drag.js'></script>
+<div id='get-myassistant' style='display:none;width:400px;text-align:left;'>
+<div class="borderwrap">
+ <div class='maintitle' id='myass-drag' title='Click and hold to drag this window'>
+  <div style='float:right'><a href='#' onclick='document.getElementById("get-myassistant").style.display="none"' title='Close Window'>[X]</a></div>
+  <div>My Assistant</div>
+ </div>
+ <div id='myass-content' style='overflow-x:auto;'></div>
+ </div>
+</div>
+<!-- Loading Layer -->
+<div id='loading-layer' style='display:none'>
+	<div id='loading-layer-shadow'>
+	   <div id='loading-layer-inner'>
+	 	<img src='style_images/1/loading_anim.gif' border='0' alt='Loading. Please Wait...' />
+		<span style='font-weight:bold' id='loading-layer-text'>Loading. Please Wait...</span>
+	    </div>
+	</div>
+</div>
+<!-- / Loading Layer -->
+<!-- Msg Layer -->
+<div id='ipd-msg-wrapper'>
+	<div id='ipd-msg-title'>
+		<a href='#' onclick='document.getElementById("ipd-msg-wrapper").style.display="none"; return false;'><img src='style_images/1/close.png' alt='X' title='Close Window' class='ipd' /></a> &nbsp; <strong>Site Message</strong>
+	</div>
+	<div id='ipd-msg-inner'><span style='font-weight:bold' id='ipd-msg-text'></span><div class='pp-tiny-text'>(Message will auto close in 2 seconds)</div></div>
+</div>
+<!-- Msg Layer -->
+
+<!-- / End board header -->
+
+<div id="userlinks">
+	<p class="home"><b>Logged in as:  <a href="http://www.storytron.com/ipb/index.php?showuser=101">billmaya</a></b> ( <a href="http://www.storytron.com/ipb/index.php?act=Login&amp;CODE=03&amp;k=478e4bcaa0e99aa3375a3c7baa577cc8">Log Out</a> )</p>
+	<p>
+
+
+
+
+
+
+	<b><a href="http://www.storytron.com/ipb/index.php?act=UserCP&amp;CODE=00" title="Edit my settings, such as signature, avatar and more...">My Controls</a></b> &middot;&nbsp;<a href="http://www.storytron.com/ipb/index.php?act=Search&amp;CODE=getnew">View New Posts</a>
+	
+		&middot;&nbsp;<a href="javascript:buddy_pop();" title="View posts since your last visit, moderator lists and more...">My Assistant</a>
+	
+	&middot;&nbsp;<a href="javascript:friends_pop();">My Friends</a>	
+
+
+	&middot;&nbsp;<a href="http://www.storytron.com/ipb/index.php?act=Msg&amp;CODE=01">0 New Messages</a>
+
+	</p>
+</div>
+<div id="navstrip"><img src='style_images/1/nav.gif' border='0'  alt='&gt;' />&nbsp;<a href='http://www.storytron.com/ipb/index.php?act=idx'>The StoryBoard</a></div>
+<!--IBF.NEWPMBOX-->
+<script language="JavaScript" type="text/javascript">
+<!--
+function contact_admin() {
+  // Very basic spam bot stopper
+	  
+  admin_email_one = 'admin';
+  admin_email_two = 'storytron.com';
+  
+  window.location = 'mailto:'+admin_email_one+'@'+admin_email_two+'?subject=Error on the forums';
+  
+}
+//-->
+</script>
+<br />
+<div class="borderwrap">
+	<h3><img src='style_images/1/nav_m.gif' border='0'  alt='&gt;' width='8' height='8' />&nbsp;Board Message</h3>
+
+<p>Sorry, an error occurred. If you are unsure on how to use a feature, or don't know why you got this error message, try looking through the help files for more information.</p>
+	<div class="errorwrap">
+		<h4>The error returned was:</h4>
+
+		<p>Sorry, but you do not have permission to use this feature. If you are not logged in, you may do so using the form below if available.</p>
+
+</div>
+
+	<!--IBF.LOG_IN_TABLE-->
+	<!--IBF.POST_TEXTAREA-->
+	<h4>Useful Links</h4>
+	<ul>
+		<li><a href="http://www.storytron.com/ipb/index.php?act=Reg&amp;CODE=10">Forgotten Password Recovery</a></li>
+		<li><a href="http://www.storytron.com/ipb/index.php?act=Reg&amp;CODE=00">Register a new account</a></li>
+		<li><a href="http://www.storytron.com/ipb/index.php?act=Help&amp;CODE=00">Our help documentation</a></li>
+		<li><a href="javascript:contact_admin();">Contact the forums administrator</a></li>
+	</ul>
+	<p class="formbuttonrow"><b><a href="javascript:history.go(-1)">Go Back</a></b></p>
+</div>
+<table cellspacing="0" id="gfooter">
+	<tr>
+		<td width="45%"><form action="http://www.storytron.com/ipb/index.php?setskin=1" name="skinselectorbox" method="post">
+	<input type="hidden" name="skinurlbits" value="act=attach&amp;type=post&amp;id=184&amp;cal_id=0" />
+	<select name="skinid" onchange="chooseskin(this)">
+		<optgroup label="Skin Selector">
+			
+<option value='2' selected="selected">IPB Classic Blue</option><!--CHILDREN:2-->
+<option value='3'>IP.Board Pro</option><!--CHILDREN:3-->
+		</optgroup>
+	</select>
+</form> <form action="http://www.storytron.com/ipb/index.php?setlanguage=1" name="langselectorbox" method="post">
+	<input type="hidden" name="langurlbits" value="act=attach&amp;type=post&amp;id=184&amp;cal_id=0" />
+	<select name="langid" onchange="chooselang(this)">
+		<optgroup label="Language">
+			
+<option value='en' selected="selected">English</option>
+		</optgroup>
+	</select>
+</form></td>
+		<td width="10%" align="center" nowrap="nowrap"><a href="lofiversion/index.php"><b>Lo-Fi Version</b></a></td>
+		<td width="45%" align="right" nowrap="nowrap">Time is now: 24th May 2009 - 04:22 PM</td>
+	</tr>
+</table>
+<script type='text/javascript'>
+//<![CDATA[
+menu_do_global_init();
+show_inline_messages();
+// Uncomment this to fix IE png images
+// causes page slowdown, and some missing images occasionally
+// if ( is_ie )
+// {
+//	 ie_fix_png();
+// }
+
+//]]>
+</script>
+ 
+<!-- Copyright Information -->
+        				  <div align='center' class='copyright'>
+        				  	Powered By <a href='http://www.invisionboard.com' style='text-decoration:none' target='_blank'>IP.Board</a>
+        				  	2.3.6 &copy; 2009 &nbsp;<a href='http://www.invisionpower.com' style='text-decoration:none' target='_blank'>IPS, Inc</a>.
+        				  </div>
+		<!-- / Copyright -->
+</div>
+</body> 
+</html>
